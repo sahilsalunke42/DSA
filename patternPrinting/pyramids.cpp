d5a7a9cdf3f4fa3ae972ed42faa5665eb528f2d5
Sahil Salunke <sahilsalunke42@gmail.com>
2026-02-15 16:00:37 +0530

    Add inverted pyramid pattern to pyramids.cpp

--- patternPrinting/pyramids.cpp
+++ patternPrinting/pyramids.cpp
@@ -15,3 +15,21 @@ int main() {
     }
     
 }
+
+// inverted pyramid
+
+
+int main() {
+    int i, j, n;
+    cin >> n;
+
+    for ( i = n; i >= 1; i--)
+    {
+        for ( j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+}
