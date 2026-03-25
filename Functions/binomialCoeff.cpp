e9fd08ab2101214bc4ad8e55d642e7929f7fc540
Sahil Salunke <sahilsalunke42@gmail.com>
2026-03-25 16:54:34 +0530

    Refactor binomial coefficient calculation by removing redundant nrFact function

--- Functions/binomialCoeff.cpp
+++ Functions/binomialCoeff.cpp
@@ -11,22 +11,13 @@ int nFact(int n){
     return ans;
 }
 
-int nrFact(int n, int r) {
-    int ans = 1;
-    for (int i = 1; i <= n-r; i++)
-    {
-        ans *= i;
-    }
-
-    return ans;
-}
 int main(){
     int n, r;
     cin >> n >> r;
     
     int nFactorial = nFact(n);
     int rFactorial = nFact(r);
-    int nrFactorial = nrFact(n, r);
+    int nrFactorial = nFact(n-r);
 
     int ans = nFactorial/(rFactorial*nrFactorial);
 
