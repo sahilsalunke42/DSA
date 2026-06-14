6d59d1c916f0ebb6bc743566717166abecac4545
Sahil Salunke <sahilsalunke42@gmail.com>
2026-06-14 16:18:14 +0530

    Fix formatting and add newline in wavePattern.cpp; separate column-wise printing logic into a new main function

--- Arrays/2DArrays/wavePattern.cpp
+++ Arrays/2DArrays/wavePattern.cpp
@@ -24,7 +24,8 @@ int main (){
         for (int j = 0; j < m; j++)
         {
             cin >> arr[i][j];
-        }     
+        }   
+        cout << endl;  
     }
     
      for (int i = 0; i < n; i++){
@@ -46,4 +47,41 @@ int main (){
         cout << endl;
      }
     
+}
+
+// colunwise now
+
+int main (){
+    int n,m;
+    cin >> n >> m;
+
+    int arr[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }   
+    }
+     cout << endl;  
+    
+     for (int j = 0; j < m; j++){
+        if (j%2 == 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                cout << arr[i][j] << " ";
+            }
+        }
+        else
+        {
+            for (int i = n-1; i >= 0; i--)
+            {
+               cout << arr[i][j] << " ";
+            }
+            
+        }
+        cout << endl;
+     }
 }
